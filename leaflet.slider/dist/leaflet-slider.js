1986933d2dcd3d8a377ea4f64db28c59c9adfd46
eclipse <eric2.pichon@gmail.com>
2015-08-22 13:47:38 +0200

    update

--- leaflet.slider/dist/leaflet-slider.js
+++ leaflet.slider/dist/leaflet-slider.js
@@ -14,13 +14,29 @@ L.Control.Slider = L.Control.extend({
         collapsed: true,
         title: 'Leaflet Slider',
         logo: 'S',
-        axis: 'horizontal'
+        orientation: 'horizontal',
+        increment: false,
+        getValue: function(value) {
+            return value;
+        },
+        showValue: true
     },
     initialize: function (f, options) {
         L.setOptions(this, options);
-        this.update = f;
-        if (this.options.axis!='vertical') {
-            this.options.axis = 'horizontal';
+        if (typeof f == "function") {
+            this.update = f;
+        } else {
+            this.update = function (value) {
+                console.log(value);
+            };
+        }
+        if (typeof this.options.getValue != "function") {
+            this.options.getValue = function (value) {
+                return value;
+            };
+        }
+        if (this.options.orientation!='vertical') {
+            this.options.orientation = 'horizontal';
         }
     },
     onAdd: function (map) {
@@ -29,21 +45,31 @@ L.Control.Slider = L.Control.extend({
         return this._container;
     },
     _updateValue: function () {
-    	this._sliderValue.innerHTML = this.value = this.slider.value;
+        if (this.options.showValue){
+    	   this._sliderValue.innerHTML = this.options.getValue(this.value = this.slider.value);
+        }
         this.update(this.value);
     },
     _initLayout: function () {
         var className = 'leaflet-control-slider';
-        this._container = L.DomUtil.create('div', 'leaflet-bar ' +className + ' ' +className + '-' + this.options.axis);
+        this._container = L.DomUtil.create('div', className + ' ' +className + '-' + this.options.orientation);
         this._sliderLink = L.DomUtil.create('a', className + '-toggle', this._container);
         this._sliderLink.setAttribute("title", this.options.title);
         this._sliderLink.innerHTML = this.options.logo;
-        
-        this._sliderValue = L.DomUtil.create('p', className+'-value', this._container);
-        this._sliderValue.innerHTML = this.options.value;
+
+        if (this.options.showValue){
+            this._sliderValue = L.DomUtil.create('p', className+'-value', this._container);
+            this._sliderValue.innerHTML = this.options.getValue(this.options.value);
+        }
+        if(this.options.increment) {
+            this._plus = L.DomUtil.create('a', className + '-plus', this._container);
+            this._plus.innerHTML = "+";
+            L.DomEvent.on(this._plus, 'click', this._increment, this);
+            L.DomUtil.addClass(this._container, 'leaflet-control-slider-incdec');
+        }
 
         this.slider = L.DomUtil.create('input', 'leaflet-slider', this._container);
-        if (this.options.axis == 'vertical') {this.slider.setAttribute("orient", "vertical");}
+        if (this.options.orientation == 'vertical') {this.slider.setAttribute("orient", "vertical");}
         this.slider.setAttribute("title", this.options.title);
         this.slider.setAttribute("id", this.options.id);
         this.slider.setAttribute("type", "range");
@@ -52,22 +78,34 @@ L.Control.Slider = L.Control.extend({
         this.slider.setAttribute("step", this.options.step);
         this.slider.setAttribute("value", this.options.value);
         this.slider.setAttribute("onChange", this.options.id + "._updateValue()");
-        if (this.options.axis =='vertical') {this.slider.style.height = (this.options.size.replace('px', '') -26) +'px';}
-        else {this.slider.style.width = (this.options.size.replace('px', '') -45) +'px';}
-        
+
+        if(this.options.increment) {
+            this._minus = L.DomUtil.create('a', className + '-minus', this._container);
+            this._minus.innerHTML = "-";
+            L.DomEvent.on(this._minus, 'click', this._decrement, this);
+        }
+
+        if (this.options.showValue){
+            if (this.options.orientation =='vertical') {this.slider.style.height = (this.options.size.replace('px','') -36) +'px';}
+            else {this.slider.style.width = (this.options.size.replace('px','') -56) +'px';}
+        } else {
+            if (this.options.orientation =='vertical') {this.slider.style.height = (this.options.size.replace('px','') -10) +'px';}
+            else {this.slider.style.width = (this.options.size.replace('px','') -25) +'px';}
+        }
+
         L.DomEvent.disableClickPropagation(this._container);
+
         if (this.options.collapsed) {
             if (!L.Browser.android) {
-                L.DomEvent.on(this._container, {
-                    mouseenter: this._expand,
-                    mouseleave: this._collapse
-                }, this);
+                L.DomEvent
+                    .on(this._container, 'mouseenter', this._expand, this)
+                    .on(this._container, 'mouseleave', this._collapse, this);
             }
 
             if (L.Browser.touch) {
                 L.DomEvent
                     .on(this._sliderLink, 'click', L.DomEvent.stop)
-                    .on(this,_sliderLink, 'click', this._expand, this);
+                    .on(this._sliderLink, 'click', this._expand, this);
             } else {
                 L.DomEvent.on(this._sliderLink, 'focus', this._expand, this);
             }
@@ -77,13 +115,19 @@ L.Control.Slider = L.Control.extend({
     },
     _expand: function () {
         L.DomUtil.addClass(this._container, 'leaflet-control-slider-expanded');
-        if(this.options.axis == 'vertical') {this._container.style.height = this.options.size;}
-        else {this._container.style.width = this.options.size;}
     },
     _collapse: function () {
         L.DomUtil.removeClass(this._container, 'leaflet-control-slider-expanded');
-        if(this.options.axis == 'vertical') {this._container.style.height = '25px';}
-        else {this._container.style.width = '26px';}
+    },
+    _increment: function () {
+        console.log(this.slider.value-this.slider.step + " " + this.slider.value+this.slider.step);
+        this.slider.value = this.slider.value*1+this.slider.step*1;
+        this._updateValue();
+    },
+    _decrement: function () {
+        console.log(this.slider.value-this.slider.step + " " + this.slider.value+this.slider.step);
+        this.slider.value = this.slider.value*1-this.slider.step*1;
+        this._updateValue();
     }
 
 
